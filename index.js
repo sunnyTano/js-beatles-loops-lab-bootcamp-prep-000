f51ad762c61d7f0c0dd31be471eb0d708fa20832
Susan Tan <susan.tan198@gmail.com>
2019-01-30 03:20:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function johnLennonFacts(facts){
   var newArr = []
   var i = 0;
   while(i < facts.length){
-    newArr.push(`${facts[i]} !!!`)
+    newArr.push(`${facts[i]}!!!`)
     i++
   }
   return newArr
