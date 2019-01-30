f31807adceff66c6958ecdee085b26ee935bc073
Susan Tan <susan.tan198@gmail.com>
2019-01-30 03:19:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,15 +9,24 @@ for(var i = 0; i<musicians.length; i++){
 }
 return newArr;
 }
+
+
+var facts = [
+        "He was the last Beatle to learn to drive",
+        "He was never a vegetarian",
+        "He was a choir boy and boy scout",
+        "He hated the sound of his own voice"
+      ]
+      
+function johnLennonFacts(facts){
+  var newArr = []
+  var i = 0;
+  while(i < facts.length){
+    newArr.push(`${facts[i]} !!!`)
+  }
+  return newArr
+}
 /*
-describe('Beatles Loops', function() {
-  it("returns an array of strings containing what instruments each musician plays", function() {
-    const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
-    expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
-  });
-
   describe('johnLennonFacts', function(){
     it("returns an array of strings with exclamation points", function() {
       expect(johnLennonFacts([
